be31814503ca8d4c4d49f1c72c64fe3ffd9448a9
Helmutai Asesoria Informatica <109847076+angaritajj@users.noreply.github.com>
2026-06-02 00:03:22 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',      // Genera la carpeta estática al compilar
   images: {
     unoptimized: true,   // Obligatorio para que Next.js te deje usar el modo 'export'
   },
